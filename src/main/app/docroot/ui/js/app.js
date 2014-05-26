609f5c13a44176b21a8bd2ba97ea21f4c4c0e98a
matiasjuan <matias.juan@gmail.com>
2014-05-26 07:41:18 -0300

    sending anypoint platform contract key

--- src/main/app/docroot/ui/js/app.js
+++ src/main/app/docroot/ui/js/app.js
@@ -50,6 +50,9 @@ app.factory('AuthInterceptor', function ($rootScope, $q, Session) {
 				var user = Session.getUser();
 				config.headers['access_token']=token;
 				config.headers['x-user-id']=user;
+				
+				//
+				config.headers['X-Anypoint-ContractKey']='538284a6e4b0624da810fad8';
 			}
 			
 			return config || $q.when(config);
